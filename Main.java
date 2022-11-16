dce0b9e5fb1475c6582e86fd84858390639a8a06
Mühle <fabian.walz@student.reutlingen-university.de>
2022-11-16 19:29:28 +0100

    board

--- Main.java
+++ Main.java
@@ -34,7 +34,6 @@ public class Main {
         array[6][3] = 1;
         array[6][6] = 1;
 
-        System.out.println("-----");
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length; j++) {
                 System.out.print(array[i][j] + "\t");
