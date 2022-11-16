40346bb36a5f19e81ca5ed50b97c2add94a03e03
Eva Kuenast <e.kuenast@gmx.net>
2022-11-17 00:31:09 +0100

    Hinzugefügte Klassen: Board Move Player Position

--- Player.java
+++ Player.java
@@ -4,6 +4,7 @@ public abstract class Player {
     protected int numGamesWon;
     protected int numPieces;
     protected int numPiecesOnBoard;
+    //protected int numPiecesToPlace;
     protected WhichPlayer whichPlayer;
     protected boolean canJump; //in Sprungphase
 
@@ -13,9 +14,9 @@ public abstract class Player {
         canJump = false;
     }
 
-    public void reset(){
-        numPiecesOnBoard = 0;
-        canJump = false;
+    //gibt den aktuellen Spieler zurueck
+    public WhichPlayer getWhichPlayer(){
+        return whichPlayer;
     }
 
     public void setName(String name){
@@ -30,8 +31,42 @@ public abstract class Player {
         return numPieces;
     }
 
+    /*
+    public int setNumPieces(){
+        this.numPieces = numPieces;
+        return numPieces;
+    }
+    */
+
     public int getNumPiecesOnBoard(){
         return numPiecesOnBoard;
     }
 
+
+    public int getNumPiecesToPlace(){
+        return (numPieces - numPiecesOnBoard);
+    }
+
+    //waehrend der Setzphase
+    public int raiseNumPiecesOnBoard(){
+        canJump = false;
+        return ++numPiecesOnBoard;
+    }
+
+    //Sprungphase beginnt
+    public int lowerNumPiecesOnBoard(){
+        if(--numPiecesOnBoard == 3){
+            canJump = true;
+        }
+        return --numPiecesOnBoard;
+    }
+
+    public boolean canJump(){
+        return canJump;
+    }
+
+    public void reset(){
+        numPiecesOnBoard = 0;
+        canJump = false;
+    }
 }
