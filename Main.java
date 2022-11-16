5094e8b2fc9e3eba87d1a2e4492960e54162ee34
Mühle <fabian.walz@student.reutlingen-university.de>
2022-11-16 19:24:02 +0100

    Just for testing git

--- Main.java
+++ Main.java
@@ -34,6 +34,7 @@ public class Main {
         array[6][3] = 1;
         array[6][6] = 1;
 
+        System.out.println("-----");
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length; j++) {
                 System.out.print(array[i][j] + "\t");
