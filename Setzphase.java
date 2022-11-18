b12b00c1eb80fdf8609cbb9ee9a3bd57e2ddffd0
Mühle <fabian.walz@student.reutlingen-university.de>
2022-11-18 15:02:21 +0100

    1.Phase Spiel

--- Setzphase.java
+++ Setzphase.java
@@ -1,30 +1,51 @@
 import java.util.Scanner;
 
-public class Setzphase {
+public class SetzphaseMuehle {
     public static void main(String[] args) {
 
-        int anzahlSteineSpieler1 = 9;
-        int anzahlSteineSpieler2 = 9;
-        String spieler1;
-        String spieler2;
+        int anzahlSteineSpieler1 = 0;
+        int anzahlSteineSpieler2 = 0;
+        String spieler1 = "Thomas";
+        String spieler2 = "Denis";
         int array[][] = new int[7][7];
 
-        System.out.println("Setze den " + anzahlSteine + ". Stein");
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Zeile:");
-        int zeile = scanner.nextInt();
-        System.out.println("Spalte:");
-        int spalte = scanner.nextInt();
+        do {
+            System.out.println(spieler1 + ", du bist an der Reihe.");
+            System.out.println("Setze deinen " + anzahlSteineSpieler1 + ". Stein");
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Zeile:");
+            int zeile = scanner.nextInt();
+            System.out.println("Spalte:");
+            int spalte = scanner.nextInt();
+            if (array[zeile][spalte] == 1) {
+                array[zeile][spalte] = 2;
+                anzahlSteineSpieler1 += 1;
+            } else if ((array[zeile][spalte] == 0) || (array[zeile][spalte] == 3)) {
+                System.out.println("Bitte ein freies Feld waehlen.");
+                int zeile = scanner.nextInt();
+                System.out.println("Spalte:");
+                int spalte = scanner.nextInt();
+                anzahlSteineSpieler1 += 1;
+            }
 
-       anzahlSteineSpieler1 -= 1;
-       anzahlSteineSpieler2 -= 1;
-
-
-        if ((anzahlSteineSpieler1 == 0) && (anzahlSteineSpieler2 == 0)) {
-            System.out.println("Die Setzphase ist beendet. Nun beginnt die Zugphase.");
+            System.out.println(spieler2 + ", du bist an der Reihe.");
+            System.out.println("Setze deinen " + anzahlSteineSpieler2 + ". Stein");
+            System.out.println("Zeile:");
+            int zeile = scanner.nextInt();
+            System.out.println("Spalte:");
+            int spalte = scanner.nextInt();
+            if (array[zeile][spalte] == 1) {
+                array[zeile][spalte] = 3;
+                anzahlSteineSpieler1 += 1;
+            } else if ((array[zeile][spalte] == 0) || (array[zeile][spalte] == 2)) {
+                System.out.println("Bitte ein freies Feld waehlen.");
+                int zeile = scanner.nextInt();
+                System.out.println("Spalte:");
+                int spalte = scanner.nextInt();
+                anzahlSteineSpieler2 += 1;
+            }
+            while ((anzahlSteineSpieler1 != 9) && (anzahlSteineSpieler2 != 9)) ;
         }
-
-
     }
 }
 
