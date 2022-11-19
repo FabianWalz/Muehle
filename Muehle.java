aceefce94a5d8ec818c3fb09c5aeb7e44d43e9c9
Mühle <fabian.walz@student.reutlingen-university.de>
2022-11-19 10:04:14 +0100

    Überprüfung auf Mühlen von Spieler1...

--- Muehle.java
+++ Muehle.java
@@ -1,5 +1,5 @@
 public class Muehle {
-    public void abfrageMuehleSpieler1(){
+    public void abfrageMuehleSpieler1() {
 
         if (array[0][0] == 2 && array[0][3] == 2 && array[0][6] == 2) {
             System.out.println("Muehle");
@@ -32,6 +32,43 @@ public class Muehle {
         } else if (array[3][4] == 2 && array[3][5] == 2 && array[3][6] == 2) {
             System.out.println("Muehle");
         } else if (array[4][3] == 2 && array[5][3] == 2 && array[6][3] == 2) {
+            System.out.println("Muehle");
+        }
+    }
+
+    public void abfrageMuehleSpieler2() {
+
+        if (array[0][0] == 3 && array[0][3] == 3 && array[0][6] == 3) {
+            System.out.println("Muehle");
+        } else if (array[0][0] == 3 && array[3][0] == 3 && array[6][0] == 3) {
+            System.out.println("Muehle");
+        } else if (array[0][6] == 3 && array[3][6] == 3 && array[6][6] == 3) {
+            System.out.println("Muehle");
+        } else if (array[6][0] == 3 && array[6][3] == 3 && array[6][6] == 3) {
+            System.out.println("Muehle");
+        } else if (array[1][1] == 3 && array[1][3] == 3 && array[1][5] == 3) {
+            System.out.println("Muehle");
+        } else if (array[1][1] == 3 && array[3][1] == 3 && array[5][1] == 3) {
+            System.out.println("Muehle");
+        } else if (array[1][5] == 3 && array[3][5] == 3 && array[5][5] == 3) {
+            System.out.println("Muehle");
+        } else if (array[5][1] == 3 && array[5][3] == 3 && array[5][5] == 3) {
+            System.out.println("Muehle");
+        } else if (array[2][2] == 3 && array[2][3] == 3 && array[2][4] == 3) {
+            System.out.println("Muehle");
+        } else if (array[2][2] == 3 && array[3][2] == 3 && array[4][2] == 3) {
+            System.out.println("Muehle");
+        } else if (array[2][4] == 3 && array[3][4] == 3 && array[4][4] == 3) {
+            System.out.println("Muehle");
+        } else if (array[4][2] == 3 && array[4][3] == 3 && array[4][4] == 3) {
+            System.out.println("Muehle");
+        } else if (array[0][3] == 3 && array[1][3] == 3 && array[2][3] == 3) {
+            System.out.println("Muehle");
+        } else if (array[3][0] == 3 && array[3][1] == 3 && array[3][2] == 3) {
+            System.out.println("Muehle");
+        } else if (array[3][4] == 3 && array[3][5] == 3 && array[3][6] == 3) {
+            System.out.println("Muehle");
+        } else if (array[4][3] == 3 && array[5][3] == 3 && array[6][3] == 3) {
             System.out.println("Muehle");
         }
     }
