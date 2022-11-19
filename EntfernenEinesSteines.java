fb853ed89073cb2a6c666072cb91c2bf72bbdcd2
Mühle <fabian.walz@student.reutlingen-university.de>
2022-11-19 19:48:08 +0100

    Stein bei Mühle entfernen

--- EntfernenEinesSteines.java
+++ EntfernenEinesSteines.java
@@ -15,7 +15,7 @@ public class EntfernenEinesSteines {
             array[zeile][spalte] = 1;
             System.out.println("Deine Eingabe war erfolgreich. Der Stein an der Stelle [" + zeile + "][" + spalte + "] wurde entfernt");
         } else if (array[zeile][spalte] != 3){
-            System.out.println("Der ausgewaehlte Stein ist ungültig. Welchen Stein moechtest du stattdessen entfernen?");
+            System.out.println("Der ausgewaehlte Stein ist ungültig. Welchen Stein möchtest du stattdessen entfernen?");
             System.out.println("Zeile: ");
             int zeileNew = scanner.nextInt();
             System.out.println("Spalte: ");
