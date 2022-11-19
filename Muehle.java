07009a490cb5aaa36e4d209421f8b36a74ecba5b
Mühle <fabian.walz@student.reutlingen-university.de>
2022-11-19 15:59:44 +0100

    Stein bei Mühle entfernen

--- Muehle.java
+++ Muehle.java
@@ -4,25 +4,8 @@ public class Muehle {   //Anweisungsblock muss noch geändert werden...
 
     int[][] array = new int[7][7];  // nur damit funktioniert
 
-    public void wegnehmenStein() {   // nur damit funktioniert
-        System.out.println("Du hast eine Muehle. Welchen Stein des Gegners möchtest du wegnehmen?");
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Zeile: ");
-        int zeile = scanner.nextInt();
-        System.out.println("Spalte: ");
-        int spalte = scanner.nextInt();
-        if (array[zeile][spalte] == 3) {
-            array[zeile][spalte] = 1;
-            System.out.println("Deine Eingabe war erfolgreich. Der Stein an der Stelle [" + zeile + "][" + spalte + "] wurde entfernt");
-        } else if (array[zeile][spalte] != 3) {
-            System.out.println("Der ausgewaehlte Stein ist ungültig. Welchen Stein moechtest du stattdessen entfernen?");
-            System.out.println("Zeile: ");
-            int zeileNew = scanner.nextInt();
-            System.out.println("Spalte: ");
-            int spalteNew = scanner.nextInt();
-            array[zeileNew][spalteNew] = 1;
-            System.out.println("Deine Eingabe war erfolgreich. Der Stein an der Stelle [" + zeileNew + "][" + spalteNew + "] wurde entfernt.");
-        }
+    public void wegnehmenStein() {
+        // EntfernenEinesSteines
     }
 
     public void abfrageMuehleSpieler1() {
